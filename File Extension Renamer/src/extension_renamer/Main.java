03a3befddcd7ffdf9e1cda158a6e15c92efb5e13
tplamondon <tplamond@sfu.ca>
2017-06-14 13:10:18 -0700

    Moved code out of main. Changed Main to use new class instead.

--- File Extension Renamer/src/extension_renamer/Main.java
+++ File Extension Renamer/src/extension_renamer/Main.java
@@ -45,66 +45,10 @@ public class Main {
 		System.out.println("Please enter the path to files: ");
 		String folderPath = scanner.nextLine(); //get the folder path
 		
-		//access the folder
-		Stream<Path> paths = null;
-		boolean error = false;
-		try{
-			//only go to depth 1 (no sub-directories)
-			paths = Files.walk(Paths.get(folderPath), 1);
-		}
-		catch(IOException e){
-			System.out.println("Folder not found. Exiting now");
-			error = true;
-		}
-		catch(SecurityException e){
-			System.out.println("Security manager denied access to the folder");
-			System.out.println("Please ensure that you have read/write permissions for this folder");
-			error = true;
-		}
-		catch(InvalidPathException e){
-			System.out.println(e.getMessage());
-			System.out.println(e.getReason());
-			System.out.println("Invalid path was entered. Exiting now");
-			error = true;
-		}
-		finally{
-			//will always execute anyways, even if no exception
-			if(error == true){
-				scanner.close();
-				System.out.println("Exiting program now");
-				System.exit(1);
-			}
-		}
-		
-		//Walk the path and select each file (as a Path object)
-		//create arrayList to know what we renamed
-		ArrayList<String> oldNames = new ArrayList<String>();
-		paths.forEach(filePath -> {
-			if (Files.isRegularFile(filePath)) {
-				//get the filename
-				String fileName = (filePath.getFileName()).toString();
-				//get the extension
-				String ext = fileName.substring(fileName.length() - 3);
-				//if it is a ogg extension, we'll want to rename the extension to ogg
-				if(oggExt(ext) == true && ext.equals("ogg") == false){
-					//add to list of files changed
-					oldNames.add(fileName);
-					//rename extension to be ogg
-					fileName = fileName.substring(0, fileName.length()-3)+"ogg";
-					//attempt to rename the file using Files.move
-					try{
-						Files.move(filePath, filePath.resolveSibling(fileName), StandardCopyOption.REPLACE_EXISTING);
-					}
-					catch(IOException e){
-						//something went wrong
-						System.out.println("Something went wrong trying to rename "+fileName.substring(fileName.length()-3));
-						System.out.println("Will continue with the rest of the files");
-					}
-				}
-			}
-		});
+		Ext_rename.rename(folderPath, "ogg", null, 1);
 		
 		//deal with results
+		/* (this is done in the thing so we're good
 		System.out.println("Finished running");
 		if(oldNames.size() == 0){
 			System.out.println("No files renamed");
@@ -115,23 +59,9 @@ public class Main {
 				System.out.println(oldNames.get(i));
 			}
 		}
+		*/
 		//close things as needed
 		scanner.close();
-		paths.close();
 	}
 	
-	//returns true if it's an "ogg" file type
-	public static boolean oggExt(String str){
-		if(str.equals("ogg")){
-			return true;
-		}
-		else if(str.equals("ogx")){
-			return true;
-		}
-		else if(str.equals("oga")){
-			return true;
-		}
-		return false;
-	}
-
 }
